dd67a7a189f831850b21f1dd4a123281f7839431
Oddo94 <razvanodenie@yahoo.com>
2019-10-29 10:17:57 +0200

    FizzBuzz Challenge Stage2

--- Curs1/src/FizzBuzz.java
+++ Curs1/src/FizzBuzz.java
@@ -4,7 +4,14 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		
 		for(int i = 1; i <= 100; i++) {
-			if(i % 3 == 0 && i % 5 == 0) {
+			
+			if(i % 7 == 0) {
+				System.out.print("Rizz,");
+				
+			}else if(i % 9 == 0) {
+				System.out.print("Jazz,");
+				
+			}else if(i % 3 == 0 && i % 5 == 0) {
 				System.out.print("FizzBuzz,");
 
 			}else if(i % 3 == 0) {
